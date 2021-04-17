3b5f6759455e25c2cf073646136f52ed81e8ae29
Ryobia <ryobia36@gmail.com>
2021-04-17 10:12:34 -0600

    finished, all routes now work as intended

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then((dbProdData) => res.json(dbProdData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then((dbProdData) => res.json(dbProdData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -90,7 +119,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
-});
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProdData) => {
+      if (!dbProdData) {
+        res.status(404).json({ message: "No Product found with this id" });
+        return;
+      }
+      res.json(dbProdData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });});
 
 module.exports = router;
